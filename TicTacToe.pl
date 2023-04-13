fdfb9a25221b6e285abbdff90e481842256e3316
Ana Mendes <ana.mendes@ice.ufjf.br>
2023-04-13 15:52:45 -0300

    Validando o modo e a quantidade de linhas

--- TicTacToe.pl
+++ TicTacToe.pl
@@ -9,14 +9,16 @@
 inicio :-
     write('Digite a quantidade de linhas do Tabuleiro: '), 
     read(Linhas),
+    validar_linhas(Linhas),
     write('Digite o modo de jogo (N ou S): '),
     read(Modo),
+    validar_modo(Modo),
     Colunas is Linhas + 1,
     criar_tabuleiro(Linhas, Colunas).
+    %pede_jogada_desafiante(Linhas, Colunas, Modo).
 
 criar_tabuleiro(Linhas, Colunas) :-
-    criar_tabuleiro_vazio(Linhas, Colunas, Tabuleiro).
-  %TODO: Pede pro user jogar.  
+    criar_tabuleiro_vazio(Linhas, Colunas, Tabuleiro). 
   %TODO:  imprime_tabuleiro(Tabuleiro).
 
 criar_tabuleiro_vazio(0, _, _) :- !.
@@ -29,3 +31,10 @@ criar_linha_vazia(0, _) :- !.
 criar_linha_vazia(Colunas, [null|LinhaAtualTabuleiro]) :-
     ProximasColunas is Colunas - 1,
     criar_linha_vazia(ProximasColunas, LinhaAtualTabuleiro).
+
+validar_linhas(Linhas) :-
+    integer(Linhas),
+    Linhas > 0.
+
+validar_modo(Modo) :-
+    (Modo == 'N'; Modo == 'S').
