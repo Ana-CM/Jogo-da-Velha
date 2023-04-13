0475468d9d8bb8bec03ceb3a3248d84a4b657362
Ana Mendes <ana.mendes@ice.ufjf.br>
2023-04-13 15:34:51 -0300

    Inciando jogo da velha

---
+++ TicTacToe.pl
@@ -0,0 +1,31 @@
+/**
+ * Ana Carolina Mendes Lino - 201865212AC
+ * Paula Rinco Rodrigues Pereira -
+ */
+
+%Importa a biblioteca XPCE para exibir o tabuleiro.
+:- use_module(library(pce)).
+
+inicio :-
+    write('Digite a quantidade de linhas do Tabuleiro: '), 
+    read(Linhas),
+    write('Digite o modo de jogo (N ou S): '),
+    read(Modo),
+    Colunas is Linhas + 1,
+    criar_tabuleiro(Linhas, Colunas).
+
+criar_tabuleiro(Linhas, Colunas) :-
+    criar_tabuleiro_vazio(Linhas, Colunas, Tabuleiro).
+  %TODO: Pede pro user jogar.  
+  %TODO:  imprime_tabuleiro(Tabuleiro).
+
+criar_tabuleiro_vazio(0, _, _) :- !.
+criar_tabuleiro_vazio(Linhas, Colunas, [LinhaAtualTabuleiro|ProximasLinhasTabuleiro]) :-
+    criar_linha_vazia(Colunas, LinhaAtualTabuleiro),
+    ProximasLinhas is Linhas - 1,
+    criar_tabuleiro_vazio(ProximasLinhas, Colunas, ProximasLinhasTabuleiro).
+
+criar_linha_vazia(0, _) :- !.
+criar_linha_vazia(Colunas, [null|LinhaAtualTabuleiro]) :-
+    ProximasColunas is Colunas - 1,
+    criar_linha_vazia(ProximasColunas, LinhaAtualTabuleiro).
