f6f995b2654791170e06dc183ea78f1a4e919383
Ana Mendes <ana.mendes@ice.ufjf.br>
2023-04-13 19:54:48 -0300

    Melhorando documentação do programa

--- TicTacToe.pl
+++ TicTacToe.pl
@@ -3,9 +3,14 @@
  * Paula Rinco Rodrigues Pereira -
  */
 
-%Importa a biblioteca XPCE para exibir o tabuleiro.
+/**
+ * Importa a biblioteca XPCE para exibir o tabuleiro.
+ */
 :- use_module(library(pce)).
 
+/**
+ * Predicado principal do programa.
+ */
 inicio :-
     write('Digite a quantidade de linhas do Tabuleiro: '), 
     read(Linhas),
@@ -14,13 +19,16 @@ inicio :-
     read(Modo),
     validar_modo(Modo),
     Colunas is Linhas + 1,
-    criar_tabuleiro(Linhas, Colunas),
+    criar_tabuleiro(Linhas, Colunas, Tabuleiro),
     Rodada is 1,
-    pede_jogada_desafiante(Linhas, Colunas, Modo, Rodada).
+    pede_jogada_desafiante(Linhas, Colunas, Modo, Rodada, Tabuleiro).
 
-criar_tabuleiro(Linhas, Colunas) :-
-    criar_tabuleiro_vazio(Linhas, Colunas, Tabuleiro). 
-  %TODO:  imprime_tabuleiro(Tabuleiro).
+
+/**
+ * Predicados relacionadas a criação do tabuleiro.
+ */
+criar_tabuleiro(Linhas, Colunas, Tabuleiro) :-
+    criar_tabuleiro_vazio(Linhas, Colunas, Tabuleiro).
 
 criar_tabuleiro_vazio(0, _, _) :- !.
 criar_tabuleiro_vazio(Linhas, Colunas, [LinhaAtualTabuleiro|ProximasLinhasTabuleiro]) :-
@@ -33,14 +41,11 @@ criar_linha_vazia(Colunas, [null|LinhaAtualTabuleiro]) :-
     ProximasColunas is Colunas - 1,
     criar_linha_vazia(ProximasColunas, LinhaAtualTabuleiro).
 
-validar_linhas(Linhas) :-
-    integer(Linhas),
-    Linhas > 0.
 
-validar_modo(Modo) :-
-    (Modo == 'N'; Modo == 'S').
-
-pede_jogada_desafiante(Linhas, Colunas, 'N', Rodada) :-
+/**
+ * Predicados relacionados as jogadas do desafiante.
+ */
+pede_jogada_desafiante(Linhas, Colunas, 'N', Rodada, Tabuleiro) :-
     write('Digite a linha da jogada: '),
     read(Linha),
     write('Digite a coluna da jogada: '),
@@ -54,7 +59,7 @@ pede_jogada_desafiante(Linhas, Colunas, 'N', Rodada) :-
     % pede_jogada_desafiante(Linhas, Colunas, 'N').
 
 
-pede_jogada_desafiante(Linhas, Colunas, 'S', Rodada) :-
+pede_jogada_desafiante(Linhas, Colunas, 'S', Rodada, Tabuleiro) :-
     write('Digite a coluna da jogada: '),
     read(Coluna),
     validar_jogada_simples(Coluna, Colunas),
@@ -64,6 +69,17 @@ pede_jogada_desafiante(Linhas, Colunas, 'S', Rodada) :-
     % verifica se o jogo acabou (validar rodada)
     % Imprime o tabuleiro
     % pede_jogada_desafiante(Linhas, Colunas, 'N').
+
+
+/**
+ * Predicados Relacionadas a validação de entradas.
+ */
+validar_linhas(Linhas) :-
+    integer(Linhas),
+    Linhas > 0.
+
+validar_modo(Modo) :-
+    (Modo == 'N'; Modo == 'S').
 
 validar_jogada_normal(Linha, Linhas, Coluna, Colunas) :-
     integer(Linha),
@@ -77,3 +93,4 @@ validar_jogada_simples(Coluna, Colunas) :-
     integer(Coluna),
     Coluna > 0,
     Coluna =< Colunas.
+
