2c0aee06497182d30e04f073bff215ed7a860f3d
Ana-CM <ana.mendes@ice.ufjf.br>
2023-04-22 19:11:25 -0300

    verifica diagonal.

--- validacoes.pl
+++ validacoes.pl
@@ -34,8 +34,6 @@ verifica_se_acabou(Rodada, Tabuleiro, Linhas, Colunas, Linha, Coluna, Simbolo, R
         Resultado = 'continua'
     ;
         verifica_sequencias(Tabuleiro, Colunas, Coluna, Simbolo, Linhas, Linhas, Linha, Resultado)
-        %verifica_sequencia_vertical(Tabuleiro, Linhas, Colunas, Simbolo, Colunas, Resultado)
-        %verifica_sequencia_diagonal(Tabuleiro, Linhas, Colunas, Simbolo, Resultado)
     ).
 
 verifica_sequencias(Tabuleiro, Colunas, Coluna, Simbolo, Final, Linhas, Linha, Resultado) :-
@@ -47,10 +45,18 @@ verifica_sequencias(Tabuleiro, Colunas, Coluna, Simbolo, Final, Linhas, Linha, R
         (NovoResultado = 'acabou' ->
             Resultado = 'acabou'
         ;
-            Resultado = 'continua'  
+           verifica_sequencia_diagonal(Tabuleiro, Colunas, Coluna, Simbolo, Final, Linhas, Linha, NovoNovoResultado),
+            (NovoNovoResultado = 'acabou' ->
+                Resultado = 'acabou'
+            ;
+                Resultado = 'continua'
+            )
         )
     ).
 
+/**
+ * Verifica se existe uma sequencia horizontal de simbolos iguais.
+ */
 verifica_sequencia_horizontal(Tabuleiro, Colunas, ColunaInicial, Simbolo, Linha, Contador, Final, Quantidade) :-
     (ColunaInicial =< Colunas, Contador < Final ->
         get_elemento(Tabuleiro, Linha, ColunaInicial, Simbolo, Resultado),
@@ -75,7 +81,6 @@ verifica_sequencia_vertical(Tabuleiro, Linhas, Coluna, Simbolo, Final, Resultado
         Resultado = 'acabou'
     ;
         Resultado = 'continua'
-        %verifica_sequencia_diagonal(Tabuleiro, Linhas, Colunas, Simbolo, Resultado)
     ).
 
 verifica_sequencia_vertical_aux(Tabuleiro, Linhas, Coluna, Simbolo, LinhaInicial, Contador, Final, Quantidade) :-
@@ -88,6 +93,88 @@ verifica_sequencia_vertical_aux(Tabuleiro, Linhas, Coluna, Simbolo, LinhaInicial
         ;
             LinhaInicialAux is LinhaInicial + 1,
             verifica_sequencia_vertical_aux(Tabuleiro, Linhas, Coluna, Simbolo, LinhaInicialAux, 0, Final, Quantidade)
+        )
+    ;
+        Quantidade = Contador
+    ).
+
+/**
+ * Verifica se existe uma sequencia diagonal de simbolos iguais.
+ */
+verifica_sequencia_diagonal(Tabuleiro, Colunas, Coluna, Simbolo, Final, Linhas, Linha, Resultado) :-
+    verifica_sequencia_diagonal_secundaria(Tabuleiro, Colunas, Linhas, Simbolo, Final, ResultadoSecundario),
+    (ResultadoSecundario = 'acabou' ->
+        Resultado = 'acabou'
+    ;
+        verifica_sequencia_diagonal_principal(Tabuleiro, Colunas, Coluna, Linhas, Linha, Simbolo, Final, ResultadoPrincipal),
+        (ResultadoPrincipal = 'acabou' ->
+            Resultado = 'acabou'
+        ;
+            Resultado = 'continua'
+        )
+    ).
+
+verifica_sequencia_diagonal_secundaria(Tabuleiro, Colunas, Linhas, Simbolo, Final, Resultado) :-
+    verifica_sequencia_diagonal_secundaria_aux(Tabuleiro, Colunas, Linhas, Simbolo, 1, 0, Final, Quantidade),
+    (Quantidade >= Final ->
+        Resultado = 'acabou'
+    ;
+        Colunas_1 is Colunas - 1,
+        verifica_sequencia_diagonal_secundaria_aux(Tabuleiro, Colunas_1, Linhas, Simbolo, 1, 0, Final, NovaQuantidade),
+        (NovaQuantidade >= Final ->
+            Resultado = 'acabou'
+        ;
+            Resultado = 'continua'
+        )
+    ).
+
+verifica_sequencia_diagonal_secundaria_aux(Tabuleiro, Colunas, Linhas, Simbolo, LinhaInicial, Contador, Final, Quantidade) :-
+    ( Colunas > 0, Linhas > 0, LinhaInicial =< Linhas, Contador =< Final ->
+        get_elemento(Tabuleiro, LinhaInicial, Colunas, Simbolo, Resultado),
+        (Resultado = 'true' ->
+            ContadorAux is Contador + 1,
+            LinhaInicialAux is LinhaInicial + 1,
+            ColunasAux is Colunas - 1,
+            verifica_sequencia_diagonal_secundaria_aux(Tabuleiro, ColunasAux, Linhas, Simbolo, LinhaInicialAux, ContadorAux, Final, Quantidade)
+        ;
+            Quantidade is 0
+        )
+    ;
+        Quantidade = Contador
+    ). 
+
+verifica_sequencia_diagonal_principal(Tabuleiro, Colunas, Coluna, Linhas, Linha, Simbolo, Final, Resultado) :-
+    ( Coluna =:= Linha ->
+        verifica_sequencia_diagonal_principal_aux(Tabuleiro, Colunas, Linhas, Simbolo, 1, 1, 0, Final, Quantidade),
+        (Quantidade >= Final ->
+            Resultado = 'acabou'
+        ;
+            Resultado = 'continua'
+        )
+    ; 
+        Linha_1 is Linha + 1,
+        ( Linha_1 =:= Coluna ->
+            verifica_sequencia_diagonal_principal_aux(Tabuleiro, Colunas, Linhas, Simbolo, 1, 2, 0, Final, NovaQuantidade),
+            (NovaQuantidade >= Final ->
+                Resultado = 'acabou'
+            ;
+                Resultado = 'continua'
+            )
+        ;
+            Resultado = 'continua'
+        )
+    ).
+
+verifica_sequencia_diagonal_principal_aux(Tabuleiro, Colunas, Linhas, Simbolo, LinhaInicial, ColunaInicial, Contador, Final, Quantidade) :-
+    ( Colunas > 0, Linhas > 0, LinhaInicial =< Linhas, ColunaInicial =< Colunas, Contador =< Final ->
+        get_elemento(Tabuleiro, LinhaInicial, ColunaInicial, Simbolo, Resultado),
+        (Resultado = 'true' ->
+            ContadorAux is Contador + 1,
+            LinhaInicialAux is LinhaInicial + 1,
+            ColunaInicialAux is ColunaInicial + 1,
+            verifica_sequencia_diagonal_principal_aux(Tabuleiro, Colunas, Linhas, Simbolo, LinhaInicialAux, ColunaInicialAux, ContadorAux, Final, Quantidade)
+        ;
+            Quantidade is 0
         )
     ;
         Quantidade = Contador
